fe4279bfa88c00af6f775782c6142e58200d1a05
zhangxiancheng <365839159@qq.com>
2021-06-08 13:10:29 +0800

    箭头函数

--- back-end/Node.js/03函数扩展.js
+++ back-end/Node.js/03函数扩展.js
@@ -78,11 +78,88 @@
     func1.apply({ obj: 1 }, [11, 22]);
 
     //合并数组
-    let num1 =[1,2,3];
-    let num2 =[4,5,6]
-    let num3=[...num1,...num2];
+    let num1 = [1, 2, 3];
+    let num2 = [4, 5, 6]
+    let num3 = [...num1, ...num2];
     console.log(num3);
 
 }
 
 //#endregion
+
+//#region   箭头函数语法
+{
+
+    //无参数无返回值
+    console.log("无参数无返回值")
+
+    let fun1 = function () {
+        console.log("null");
+    }
+    fun1();
+
+    console.log("------------------------------------------");
+
+    let fun11 = () => console.log("null");
+    fun11();
+    //带参数
+    console.log("带参数")
+
+    let fun2 = function (x = 0) {
+        console.log(x);
+    }
+    fun2(2);
+
+    console.log("------------------------------------------");
+
+    let fun22 = (x = 1) => console.log(x);
+    fun22(22)
+
+    //带返回值
+    console.log("带返回值")
+
+    let fun3 = function () {
+        return "fun3";
+    }
+    console.log(fun3())
+
+    console.log("------------------------------------------");
+
+    let fun33 = () => "fun33";
+    console.log(fun33());
+    //带参数、带返回值
+    console.log("带参数、带返回值")
+
+    let fun4 = function (a, b) {
+        return a + b;
+    }
+    console.log(fun4(1, 2));
+
+    console.log("------------------------------------------");
+
+    let fun44 = (a, b) => a + b;
+    console.log(fun44(3, 4));
+
+    //简化匿名函数
+    console.log("简化匿名函数")
+    let arr = [1, 2, 3];
+    arr.forEach(function (value, index, array) {
+
+        console.log(value);
+    });
+    
+    console.log("------------------------------------------");
+
+    arr.forEach((value, index, array) => console.log(value));
+    
+    console.log("使用 rest（...values）代替arguments")
+
+    let temp = (...values) =>console.log(values);
+    temp(1,2,3);
+}
+//注：（1）箭头函数没有自己的作用域、箭头函数使用的this 都是使用外部的函数的this
+//    （2）箭头函数不能new =》let fun=()=>{this.a=123}; new fun() Error :fun is not a constructor
+//     （3）箭头函数不能使用 arguments 获取参数列表  可以使用 rest（...values）代替 
+//#endregion
+
+
