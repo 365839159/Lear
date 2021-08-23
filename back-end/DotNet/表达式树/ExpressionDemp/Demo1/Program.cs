3fd7187786c9c465de876f5cfc768006680e7502
365839159 <365839159@qq.com>
2021-08-23 22:00:17 +0800

    zxc

---
+++ back-end/DotNet/表达式树/ExpressionDemp/Demo1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World!");
+           File.OpenText("");
+           List<int> list = new List<int>();
+           list.Add(1);
+           foreach (var i in list.GroupBy(s=>s))
+           {
+             
+           }
+
+           var ss= list.GroupBy(s => s).Select(s => s);
+        }
+    }
+}
